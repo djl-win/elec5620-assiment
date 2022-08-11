7affc477cb801923ecd0e1309eeebfb3b38c04d7
djl-win <day.dong99@yahoo.com>
2022-08-11 14:55:10 +1000

    add database file : table tb_userdetail

---
+++ db_data/tb_userdetail.sql
@@ -0,0 +1,11 @@
+drop table if exists tb_userdetail;
+create table tb_userdetail(
+		userdetail_id serial primary key,
+		userdetail_name varchar(30),
+		userdetail_email varchar(30),
+		userdetail_phone varchar(20),
+		userdetail_deleted integer default 0,
+		userdetail_userid integer,
+		constraint FK1_tb_userdetail foreign key(userdetail_userid) references tb_user(user_id) on update cascade on delete cascade
+);
+
